6943dc6a1b5e928430bad669cfa0b5c08def68a6
Nikita034034 <dmitrenko.n955@gmail.com>
2025-05-27 22:56:41 +0300

    Create IntegerComparator.java

---
+++ src/main/java/IntegerComparator.java
@@ -0,0 +1,11 @@
+public class IntegerComparator {
+    public int compare(int a, int b) {
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+} 
