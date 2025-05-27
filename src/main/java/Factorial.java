abff73a744169afed8109c5cfb4ebb5d84bab713
Nikita034034 <dmitrenko.n955@gmail.com>
2025-05-27 22:56:28 +0300

    Create Factorial.java

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public int calculate(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+} 
